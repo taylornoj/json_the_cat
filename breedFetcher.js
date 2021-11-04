c15526698198d3743db5b7b5d2cc8928c11c4a01
Taylor Noj <taylornoj@gmail.com>
2021-11-04 22:45:18 +0000

    refactor and index added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,41 @@
 const request = require('request');
-const breedName = process.argv.slice(2);
 
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName[0]}`, function(error, response, body) {
+const fetchBreedDescription = function(breedName, callback) {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
   if (error) {
-    return console.log("Failed to request details:", error);
+    callback (error, null)
   }
 
 
   const data = JSON.parse(body);
-  
+
   if (data[0] === undefined) {
-    console.log(`${breedName} not found`);
+    callback(null, `${breedName} not found`);
   } else {
-    //console.log(data);
-    console.log(data[0]['description']);
+    callback(null, data[0]['description']);
   }
 });
+};
+
+module.exports = { fetchBreedDescription };
+
+
+
+
+
+
+
+
+// request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName[0]}`, function(error, response, body) {
+//   if (error) {
+//     return console.log("Failed to request details:", error);
+//   }
+
+
+//   const data = JSON.parse(body);
+//   if (data[0] === undefined) {
+//     console.log(`${breedName} not found`);
+//   } else {
+//     console.log(data[0]['description']);
+//   }
+// });
