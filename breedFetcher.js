8282c5f643ce25ed9c5edc4ef7ee1599a3633e36
Taylor Noj <taylornoj@gmail.com>
2021-11-04 18:14:48 +0000

    used JSON.parse to convert JSON string into object

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,9 @@
 const request = require('request'); 
+
+request('https://api.thecatapi.com/v1/breeds/search?q=sib', function (error, response, body) {
+  //console.log('body:', body);
+  //console.log(typeof body);
+  const data = JSON.parse(body);
+console.log(data);
+console.log(typeof data);
+})
