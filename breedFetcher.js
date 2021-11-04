2b334ea2bd04e7d6cead5f5e1bc5b009ccc5c60c
Taylor Noj <taylornoj@gmail.com>
2021-11-04 20:09:47 +0000

    added ability to pull breed from command line

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,14 @@
 const request = require('request'); 
+const args = process.argv.slice(2);
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', function (error, response, body) {
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, function (error, response, body) {
   //console.log('body:', body);
   //console.log(typeof body);
   const data = JSON.parse(body);
   
-  console.log(data[0]['description']);
-//console.log(data);
+  //console.log(data[0]['description']);
+console.log(data);
 //console.log(typeof data);
 
 });
